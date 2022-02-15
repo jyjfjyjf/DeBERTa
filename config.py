5943cfce87853225b18099b27791a7ba65139d04
JYN <woaituililixing1@163.com>
2022-02-15 14:57:43 +0800

    添加参数

--- config.py
+++ config.py
@@ -1,14 +1,20 @@
 import os
 root_path = os.path.abspath(os.path.dirname(__file__))
-model_dir = os.path.join(root_path, 'lib\\deberta-v2-xlarge-mnli')
+model_dir = os.path.join(root_path, 'lib\\deberta-v2-xlarge')
 vocab_path = os.path.join(model_dir, 'spm.model')
 model_path = os.path.join(model_dir, 'pytorch_model.bin')
 paddle_model_path = os.path.join(model_dir, 'model_state.pdparams')
 batch_size = 1
-valid_batch_size = 4
+valid_batch_size = 32
 max_length = 128
 lr = 4e-6
 adv_weight = 5  # 对抗学习权重
 seed = 2022369
 
+train_data_path = os.path.join(root_path, 'data\\MNLI\\train.tsv')
+valid_data_path = os.path.join(root_path, 'data\\MNLI\\dev_matched.tsv')
+
+mnli_label2id = {'contradiction': 0, 'entailment': 2, 'neutral': 1}
+mnli_id2label = {0: 'contradiction', 2: 'entailment', 1: 'neutral'}
+device = 'cuda'
 
